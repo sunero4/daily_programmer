e260c9675148862ae2be89a5e4e420cdf4297cf3
Anders <anderskloborg@gmail.com>
2019-01-27 10:39:00 +0100

    Number 372 with bonus

---
+++ DailyProgrammer/PerfectlyBalanced372/PerfectlyBalanced.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgrammer.PerfectlyBalanced372
+{
+    public class PerfectlyBalanced
+    {
+        public bool IsPerfectlyBalanced(string characters)
+        {
+            var countingArray = new int[26];
+
+            if(characters.Length <= 0)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                countingArray[characters[i] - 97]++;
+            }
+
+            var occuringCharacters = countingArray.Where(x => x > 0).ToArray();
+
+            for (int i = 0; i < occuringCharacters.Length - 1; i++)
+            {
+                if(occuringCharacters[i] != occuringCharacters[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- DailyProgrammer/Program.cs
+++ DailyProgrammer/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DailyProgrammer.ClosestString353;
 using DailyProgrammer.RepeatingNumbers341;
 using DailyProgrammer.TheRabbitProblem348;
+using DailyProgrammer.PerfectlyBalanced372;
 
 namespace DailyProgrammer
 {
@@ -13,42 +14,6 @@ namespace DailyProgrammer
     {
         static void Main(string[] args)
         {
-            //var rabbits = new RabbitBreedingCalculator();
-            //var result = rabbits.MonthsBeforeWorldDomination(2, 4, 15000000000);
-
-            //rabbits.PrintResult(result.Item1, result.Item2);
-
-            //var repeatingNumbers = new RepeatingNumbers();
-            //var results = repeatingNumbers.FindRepeatingNumbers("82156821568221");
-
-            //foreach (var repeatedNumber in results)
-            //{
-            //    Console.WriteLine("The number {0} was repeated {1} times.", repeatedNumber.Number, repeatedNumber.NumberOfOccurrences);
-            //}
-            //Console.ReadLine();
-
-            var closestStringCalculator = new ClosestStringFinder(new HammingDistance());
-            var strings = new string [11]
-            {
-                "AACACCCTATA", "CTTCATCCACA", "TTTCAATTTTC", "ACAATCAAACC", "ATTCTACAACT",
-                "ATTCCTTATTC", "ACTTCTCTATT", "TAAAACTCACC", "CTTTTCCCACC", "ACCTTTTCTCA", "TACCACTACTT"
-            };
-            var result1 = closestStringCalculator.FindClosestString(strings);
-            Console.WriteLine(result1);
-
-            var strings2 = new string[21]
-            {
-                "ACAAAATCCTATCAAAAACTACCATACCAAT", "ACTATACTTCTAATATCATTCATTACACTTT", "TTAACTCCCATTATATATTATTAATTTACCC",
-                "CCAACATACTAAACTTATTTTTTAACTACCA", "TTCTAAACATTACTCCTACACCTACATACCT", "ATCATCAATTACCTAATAATTCCCAATTTAT",
-                "TCCCTAATCATACCATTTTACACTCAAAAAC", "AATTCAAACTTTACACACCCCTCTCATCATC", "CTCCATCTTATCATATAATAAACCAAATTTA",
-                "AAAAATCCATCATTTTTTAATTCCATTCCTT", "CCACTCCAAACACAAAATTATTACAATAACA", "ATATTTACTCACACAAACAATTACCATCACA",
-                "TTCAAATACAAATCTCAAAATCACCTTATTT", "TCCTTTAACAACTTCCCTTATCTATCTATTC", "CATCCATCCCAAAACTCTCACACATAACAAC",
-                "ATTACTTATACAAAATAACTACTCCCCAATA", "TATATTTTAACCACTTACCAAAATCTCTACT", "TCTTTTATATCCATAAATCCAACAACTCCTA",
-                "CTCTCAAACATATATTTCTATAACTCTTATC", "ACAAATAATAAAACATCCATTTCATTCATAA", "CACCACCAAACCTTATAATCCCCAACCACAC",
-            };
-            var result2 = closestStringCalculator.FindClosestString(strings2);
-            Console.WriteLine(result2);
-            Console.ReadLine();
         }
     }
 }
